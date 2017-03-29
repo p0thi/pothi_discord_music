d62745ddc88d25fc72f73a5c6a18309ea8ea952a
Pothi <pascal.pothmann@gmail.com>
2017-03-29 11:17:59 +0200

    Statistics can now be accessed by others with permissions

--- src/main/java/pothi_discord/bots/music/commands/fun/StatisticsCommand.java
+++ src/main/java/pothi_discord/bots/music/commands/fun/StatisticsCommand.java
@@ -1,11 +1,14 @@
 package pothi_discord.bots.music.commands.fun;
 
 import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import pothi_discord.bots.BotShard;
 import pothi_discord.commands.GuildCommand;
 import pothi_discord.handlers.MessageDeleter;
+import pothi_discord.permissions.Permission;
+import pothi_discord.permissions.PermissionManager;
 import pothi_discord.utils.HugeMessageSender;
 import pothi_discord.utils.TextUtils;
 import pothi_discord.utils.database.morphia.userdata.Gametime;
@@ -28,10 +31,9 @@ public class StatisticsCommand extends GuildCommand{
         }
         event.getChannel().sendTyping().queue();
 
-        Userdata userdata = Userdata.getUserdata(user.getId());
-
-
         int days = -1;
+
+        User userToCheck = user;
 
         if (args.length > 1 && args[1].matches("\\d+")) {
             try {
@@ -40,11 +42,21 @@ public class StatisticsCommand extends GuildCommand{
                 event.getChannel().sendMessage("Der Zeitraum ist ungültig." + " :see_no_evil:")
                         .queue(new MessageDeleter());
             }
+            if (args.length > 2
+                    && PermissionManager.checkUserPermission(guild, user, "get-statistics-of-others")) {
+                Member tmpMember = guild.getMemberById(args[2]);
+                if (tmpMember != null) {
+                    userToCheck = tmpMember.getUser();
+                }
+            }
         }
+
+        Userdata userdata = Userdata.getUserdata(userToCheck.getId());
+
         String currentModifiedDate = TextUtils.millisToDate(System.currentTimeMillis(), "-");
 
         HugeMessageSender hugeMessageSender = new HugeMessageSender();
-        hugeMessageSender.setHeader("Alle registrierten Spielzeiten von " + user.getName()
+        hugeMessageSender.setHeader("Alle registrierten Spielzeiten von " + userToCheck.getName()
                 + (days > -1 ? (days == 1 ? " von heute$MULTIPAGE$:" : " in den letzten " + days + " Tagen$MULTIPAGE$:") : "$MULTIPAGE$:"));
         hugeMessageSender.setTag(HugeMessageSender.MessageTag.INFO);
         hugeMessageSender.setMpInfix(". Seite $CURRENTPAGE$/$TOTALPAGES$");
