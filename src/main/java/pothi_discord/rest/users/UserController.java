fd7064c5794781e2d57e76594b5c51f3d8e50cbd
Pothi <pascal.pothmann@gmail.com>
2017-07-14 20:00:39 +0200

    Endpoint to rename Playlist added

--- src/main/java/pothi_discord/rest/users/UserController.java
+++ src/main/java/pothi_discord/rest/users/UserController.java
@@ -5,19 +5,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.omg.CORBA.BAD_PARAM;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pothi_discord.bots.music.commands.audio.PlaylistCommand;
 import pothi_discord.rest.auth.AuthController;
 import pothi_discord.utils.Param;
 import pothi_discord.utils.database.morphia.MongoAudioTrack;
 import pothi_discord.utils.database.morphia.userdata.UserPlaylist;
 import pothi_discord.utils.database.morphia.userdata.Userdata;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 
 /**
  * Created by Pascal Pothmann on 30.06.2017.
@@ -163,6 +163,65 @@ public class UserController {
         }
         else {
             return ResponseEntity.ok(new JSONObject().put("message", "All matching entries removed").toString());
+        }
+    }
+
+    @RequestMapping(value = "/userplaylist/rename", method = RequestMethod.PATCH)
+    public Object renameUserplaylists(@RequestParam(value = "id") String playlistId,
+                                           @RequestParam(value = "name") String name,
+                                           @RequestParam Map<String, String> requestParams,
+                                           @RequestHeader Map<String, String> headers) {
+        String exceptionString = AuthController.getAuthorizationErrorString(headers, requestParams);
+
+        if (exceptionString != null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new JSONObject().put("message", exceptionString).toString());
+        }
+
+        Matcher matcher = PlaylistCommand.PLAYLIST_NAME_PATTERN.matcher(name);
+        if (!matcher.find()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(String.format("Invalid name. It has to be between %d and %d letters",
+                            PlaylistCommand.MIN_PLAYLIST_NAME_LENGTH,
+                            PlaylistCommand.MAX_PLAYLIST_NAME_LENGTH));
+        }
+
+        String token = AuthController.getToken(headers, requestParams);
+        String userId = Jwts.parser().setSigningKey(Param.SECRET_KEY()).parseClaimsJws(token).getBody().getSubject();
+
+        Userdata userdata = Userdata.getUserdata(userId);
+        if (userdata == null) {
+            return ResponseEntity.ok("[]");
+        }
+
+        List<UserPlaylist> userPlaylists = userdata.getPlaylists();
+        UserPlaylist myPlaylist = null;
+        boolean nameALreadyTaken = false;
+        for (UserPlaylist userPlaylist : userPlaylists) {
+            if (userPlaylist.getId().toHexString().equals(playlistId)) {
+                myPlaylist = userPlaylist;
+                nameALreadyTaken = myPlaylist.getName().equals(name);
+                break;
+            }
+        }
+        if (myPlaylist == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No playlist with this id");
+        }
+
+        if (nameALreadyTaken) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Name already taken");
+        }
+
+        String oldName = myPlaylist.getName();
+        myPlaylist.setName(name);
+
+        if(myPlaylist.saveInstance() == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Server error. Please try again");
+        }
+        else {
+            return ResponseEntity.ok(new JSONObject()
+                    .put("message", String.format("Playlist %s renamed to %s", oldName, myPlaylist.getName()))
+                    .toString());
         }
     }
 }
