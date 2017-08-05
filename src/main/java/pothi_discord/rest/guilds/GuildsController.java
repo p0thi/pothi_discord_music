95bb2b9ac262672e5c82531ccb4b1bd6e262b5d2
Pothi <pascal.pothmann@gmail.com>
2017-08-05 20:22:59 +0200

    added endpoint to play audio commands

--- src/main/java/pothi_discord/rest/guilds/GuildsController.java
+++ src/main/java/pothi_discord/rest/guilds/GuildsController.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.VoiceChannel;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
@@ -13,11 +14,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pothi_discord.Main;
 import pothi_discord.bots.BotShard;
+import pothi_discord.bots.sound.commands.audio.PlayerCommand;
+import pothi_discord.bots.sound.commands.controll.CommandsCommand;
+import pothi_discord.listeners.TrackScheduler;
+import pothi_discord.permissions.PermissionManager;
 import pothi_discord.rest.auth.AuthController;
 import pothi_discord.utils.Param;
 import pothi_discord.utils.database.morphia.guilddatas.GuildData;
+import pothi_discord.utils.database.morphia.guilddatas.SoundCommandEntry;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -93,7 +100,84 @@ public class GuildsController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing id query string");
         }
 
-        return "";
+        String token = AuthController.getToken(headers, requestParams);
+
+        String userId = Jwts.parser().setSigningKey(Param.SECRET_KEY()).parseClaimsJws(token).getBody().getSubject();
+        String guildId = requestParams.get("guildId");
+
+        Guild guild = Main.soundBot.getGuildById(guildId);
+
+        if (guild == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Could not find this guild");
+        }
+
+        User user = guild.getMemberById(userId).getUser();
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST). body("You have to be in this guild.");
+        }
+
+        boolean canUserShowCommands = PermissionManager.checkUserPermission(guild, user, new CommandsCommand().getName());
+
+        if (!canUserShowCommands) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You don't have the permissions to see the commands");
+        }
+
+        GuildData guildData = GuildData.getGuildDataByGuildId(guildId);
+        List<SoundCommandEntry> commands = guildData.getSoundCommands().getSoundCommandEntries();
+
+        JSONArray jsonArray = new JSONArray(commands);
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            jsonArray.getJSONObject(i).remove("fileId");
+        }
+
+        return ResponseEntity.ok(jsonArray.toString(2));
+    }
+
+    @RequestMapping(value = "/guilds/soundcommands/play/{soundcommand}", method = RequestMethod.POST)
+    public Object playSoundcommand(@RequestParam Map<String, String> requestParams,
+                                        @RequestHeader Map<String, String> headers,
+                                        @PathVariable("soundcommand") String soundcommand) {
+
+        String error = AuthController.getAuthorizationErrorString(headers, requestParams);
+
+        if (error != null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+        }
+
+        if (!requestParams.containsKey("guildId")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing id query string");
+        }
+
+        String token = AuthController.getToken(headers, requestParams);
+
+        String userId = Jwts.parser().setSigningKey(Param.SECRET_KEY()).parseClaimsJws(token).getBody().getSubject();
+        String guildId = requestParams.get("guildId");
+
+        Guild guild = Main.soundBot.getGuildById(guildId);
+
+        if (guild == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Could not find this guild");
+        }
+
+        User user = guild.getMemberById(userId).getUser();
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST). body("You have to be in this guild.");
+        }
+
+        VoiceChannel voiceChannel = guild.getMember(user).getVoiceState().getChannel();
+
+        if (!PlayerCommand.cooldownAllowed(guild, user)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You have to wait before you can trigger more commands.");
+        }
+
+        TrackScheduler scheduler = Main.soundBot.getGuildAudioPlayer(guild).getScheduler();
+        PlayerCommand playerCommand = new PlayerCommand(soundcommand, false);
+        playerCommand.action(guild, user, voiceChannel, scheduler);
+
+        return ResponseEntity.ok(new JSONObject().put("message", "playing...").toString());
     }
 
     public static boolean isUserInGuild(String guildId, String userId) {
